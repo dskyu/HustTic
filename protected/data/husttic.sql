211957084c84d0daa220b660785569345750770c
Peiwen Wang <wangpeiwen19920808@gmail.com>
2013-01-24 18:29:45 +0800

    新数据库 有待增加详细信息字段

--- protected/data/husttic.sql
+++ protected/data/husttic.sql
@@ -1,170 +1,106 @@
--- phpMyAdmin SQL Dump
--- version 3.2.4
--- http://www.phpmyadmin.net
---
--- 主机: localhost
--- 生成日期: 2012 年 12 月 16 日 01:34
--- 服务器版本: 5.1.62
--- PHP 版本: 5.3.8
-
-SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+drop database if exists husttic;
+create database husttic;
+use husttic
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
-
---
--- 数据库: `husttic`
---
-
--- --------------------------------------------------------
-
---
--- 表的结构 `admin`
---
-
-CREATE TABLE IF NOT EXISTS `admin` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `password` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- 转存表中的数据 `admin`
---
-
-
--- --------------------------------------------------------
-
---
--- 表的结构 `config`
---
-
-CREATE TABLE IF NOT EXISTS `config` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `copyright` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `place` varchar(255) NOT NULL,
-  `bbs_url` varchar(255) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- 转存表中的数据 `config`
---
-
-
--- --------------------------------------------------------
-
---
--- 表的结构 `group`
---
-
+DROP TABLE IF EXISTS `group`;
 CREATE TABLE IF NOT EXISTS `group` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `leader_id` int(11) NOT NULL,
-  `leader_name` varchar(255) NOT NULL,
-  `vice_leader_id` int(11) DEFAULT NULL,
-  `vice_leader_name` varchar(255) DEFAULT NULL,
   `name` varchar(255) NOT NULL,
+  description text,
+  /*
+  描述可以存html格式
+  还有没有别的部门相关信息
+  */
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- 转存表中的数据 `group`
---
-
-
--- --------------------------------------------------------
-
---
--- 表的结构 `message`
---
-
-CREATE TABLE IF NOT EXISTS `message` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `is_anonymity` tinyint(1) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- 转存表中的数据 `message`
---
-
-
--- --------------------------------------------------------
-
---
--- 表的结构 `news`
---
-
-CREATE TABLE IF NOT EXISTS `news` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `title` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `author_id` int(11) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- 转存表中的数据 `news`
---
-
-
--- --------------------------------------------------------
-
---
--- 表的结构 `project`
---
-
-CREATE TABLE IF NOT EXISTS `project` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(255) NOT NULL,
-  `content` text NOT NULL,
-  `time` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `author_names` varchar(255) NOT NULL,
-  `author_ids` int(11) DEFAULT NULL,
-  `url` varchar(255) DEFAULT NULL,
-  `group_id` int(11) DEFAULT NULL,
-  `group_name` varchar(255) DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
-
---
--- 转存表中的数据 `project`
---
-
-
--- --------------------------------------------------------
-
---
--- 表的结构 `user`
---
-
+DROP TABLE IF EXISTS `user`;
 CREATE TABLE IF NOT EXISTS `user` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
-  `email` varchar(255) NOT NULL,
-  `sex` tinyint(1) NOT NULL,
-  `birthday` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `group_id` int(11) NOT NULL,
-  `group_name` varchar(255) NOT NULL,
-  `title` varchar(255) DEFAULT NULL,
-  `grade` int(11) NOT NULL,
-  `class` int(11) NOT NULL,
-  `college` varchar(255) NOT NULL,
+  `level` int(1) DEFAULT 0,
+  `description` text,
+  /*
+  另外一些个人信息什么的
+  */
+  prigroup_id int(11) not null,
+  foreign key(prigroup_id)references `group`(id),
   PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
---
--- 转存表中的数据 `user`
---
+DROP TABLE IF EXISTS `user_group`;
+CREATE TABLE IF NOT EXISTS `user_group` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  user_id int(11) not null,
+  group_id int(11) not null,
+  foreign key(user_id)references `user`(id),
+  foreign key(group_id)references `group`(id),
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
 
+DROP TABLE IF EXISTS `project`;
+CREATE TABLE IF NOT EXISTS `project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  description text,
+  /*
+  项目展示时间上用不用提是什么时候的
+  或者一个大概的时间吧
+  下面这个是年份....
+  */
+  `year` int(4),  
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+DROP TABLE IF EXISTS `user_project`;
+CREATE TABLE IF NOT EXISTS `user_project` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `user_id` int(11) NOT NULL,
+  `project_id` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  foreign key(user_id) references `user`(id),
+  foreign key(project_id) references project(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/*站内信*/
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE IF NOT EXISTS `message` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `source_id` int(11) NOT NULL,
+  `dest_id` int(11) NOT NULL,
+  `content` text,
+  `time` timestamp not null,
+  `state` int(1) not null default 0,/*已读未读之类的状态*/
+  `type` int(1) not null default 0,
+  PRIMARY KEY (`id`),
+  foreign key(source_id) references `user`(id),
+  foreign key(dest_id) references `user`(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+DROP TABLE IF EXISTS `award_punish`;
+CREATE TABLE IF NOT EXISTS `award_punish` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `source_id` int(11) NOT NULL,
+  `dest_id` int(11) NOT NULL,
+  `content` text,
+  `level` int(1) not null,
+  `time` timestamp not null,
+  PRIMARY KEY (`id`),
+  foreign key(source_id) references `user`(id),
+  foreign key(dest_id) references `user`(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
+
+DROP TABLE IF EXISTS `announce`;
+CREATE TABLE IF NOT EXISTS `announce` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `group_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `title` varchar(255) NOT NULL,
+  `description` text not null,
+  `time` timestamp not null,
+  PRIMARY KEY (`id`),
+  foreign key(user_id)references `user`(id),
+  foreign key(group_id)references `group`(id)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1;
